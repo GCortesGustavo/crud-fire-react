286df972681fde285df36a32f8c939aacac851df
Gustavo Cortes <gustavocortes934@gmail.com>
2024-03-08 18:04:34 -0300

    Log

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+//importamos nuestros componentes
 import Show from './components/Show';
+import Create from './components/Create';
+import Edit from './components/Edit';
+
+//importamos el router
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">   
-      <h1>Hola Firebase</h1>
-
-      <Show />
+    <div className="App">    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Show /> } />
+        <Route path='/create' element={ <Create /> } />
+        <Route path='/edit/:id' element={ <Edit /> } />
+      </Routes>
+    </BrowserRouter> 
     </div>
   );
 }
